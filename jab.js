792ee5663f6ba90f48b7689136ab24859c45280d
victor <joaovas123m@gmail.com>
2026-06-25 17:08:24 -0300

    Novas modificações feitas
    os arquivos .html, .css e .js foram alterados emodificados. a logica de manipulação de DOM do js foi aprimorado

--- jab.js
+++ jab.js
@@ -9,6 +9,24 @@ toggleIconBtn.forEach(element => {
         } else {
             details.style.display = 'none';
         }
-    })
+    });
 });
 
+const contracUsBtn = document.querySelectorAll('.contract-us-btn');
+
+contracUsBtn.forEach(pages => {
+    pages.addEventListener('click', function () {
+        const pagesToHide = document.querySelectorAll(
+            '.homepage, .services-page, .specialties-page, .about-jab-page, .jab-leaders-page'
+        );
+        const pageToShow = document.querySelector('.contract-page');
+
+        if (pageToShow.style.display === 'block') {
+            pageToShow.style.display = 'none';
+            pagesToHide.forEach(page => page.style.display = 'block');
+        } else {
+            pageToShow.style.display = 'block';
+            pagesToHide.forEach(page => page.style.display = 'none');
+        }
+    });
+});
